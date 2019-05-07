bb70923cb03124ebe7e060b92dbaf4f82519b20a
Sara <sarakokvv@gmail.com>
2019-05-07 09:44:25 +0200

    Versie 1

---
+++ opdracht1/v3/filmopdracht.js
@@ -0,0 +1,21 @@
+var opslaan = document.querySelector('.save');
+
+
+function saving() {
+  opslaan.classList.add("saved");
+	
+}
+
+opslaan.addEventListener('click', saving);
+
+
+
+var hartje = document.querySelector('.hearticon');
+
+
+function addOpslaan() {
+	
+  hartje.classList.add("heart");
+}
+
+opslaan.addEventListener('click', addOpslaan);
